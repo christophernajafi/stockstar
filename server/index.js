7b8a96768b624d5b6914e4c1a7ea4767f5c9d0c2
Christopher Najafi <chris.najafi@gmail.com>
2020-02-29 16:24:37 -0500

    trying path.resolve for static serving

--- server/index.js
+++ server/index.js
@@ -21,11 +21,12 @@ if (process.env.NODE_ENV === "production") {
   //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
   // );
 
-  app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static("client/build"));
+  // app.use(express.static(path.join(__dirname, "client/build")));
 
-  // -app.get('/', function (req, res) {
   app.get("/*", function(req, res) {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    // res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
 }
 
