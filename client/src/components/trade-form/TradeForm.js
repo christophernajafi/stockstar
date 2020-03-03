a04373467416eb0fde01910c4bed97ebc21f2ece
Christopher Najafi <chris.najafi@gmail.com>
2020-03-02 20:48:46 -0500

    now have trade form on portfolio page, console logs trade submission

--- client/src/components/trade-form/TradeForm.js
+++ client/src/components/trade-form/TradeForm.js
@@ -1,14 +1,12 @@
 import React, { Fragment, useState } from "react";
-import { Button, Form } from "react-bootstrap";
+import { Button, Form, Container } from "react-bootstrap";
 
 import "./trade-form.css";
 
 const TradeForm = props => {
   const [formState, setFormState] = useState({
-    type: "",
-    quantity: null,
-    stockSymbol: "",
-    price: null
+    ticker: "",
+    quantity: ""
   });
 
   const handleChange = event => {
@@ -20,17 +18,49 @@ const TradeForm = props => {
 
   const handleSubmit = event => {
     event.preventDefault();
-    props.history.push("/portfolio");
+    // props.history.push("/portfolio");
     console.log("formState: ", formState);
   };
+
+  // const { classes, userCashBalance, error } = this.props;
+
+  const isValidQuantity =
+    Number(formState.quantity) > 0 && !String(formState.quantity).includes(".");
+
+  const isEmptyTicker = formState.ticker === "";
 
   return (
     <Fragment>
       <div className="trade-box">
-        Cash Balance: $
-        <br />
-        {/* <button className="btn btn-primary">Buy</button> */}
-        <Button>Buy</Button>
+        <Container>
+          Cash Balance: $
+          <br />
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="ticker">
+              <Form.Label>Ticker Symbol</Form.Label>
+              <Form.Control
+                name="ticker"
+                type="text"
+                placeholder="Enter ticker symbol"
+                onChange={handleChange}
+                value={formState.ticker}
+              />
+            </Form.Group>
+            <br />
+            <Form.Group controlId="quantity">
+              <Form.Label>Number of Shares</Form.Label>
+              <Form.Control
+                name="quantity"
+                value={formState.quantity}
+                type="text"
+                placeholder="Enter quantity"
+                onChange={handleChange}
+              />
+            </Form.Group>
+            {/* <button className="btn btn-primary">Buy</button> */}
+            <Button type="submit">Buy</Button>
+          </Form>
+        </Container>
       </div>
     </Fragment>
   );
--- client/src/store/reducers/authReducer.js
+++ client/src/store/reducers/authReducer.js
@@ -1,10 +1,48 @@
 import axios from "axios";
 
+const REGISTER_SUCCESS = "REGISTER_SUCCESS";
+const REGISTER_FAIL = "REGISTER_FAIL";
+const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+const LOGIN_FAIL = "LOGIN_FAIL";
+const LOGOUT = "LOGOUT";
+
 // register
+export const register = registrationData => async dispatch => {
+  try {
+    await axios.post("api/users/", registrationData);
+    await authorize();
+    // dispatch({
+    //   type: REGISTER_SUCCESS
+    // });
+  } catch (err) {
+    console.log(err.message);
+  }
+};
 
 // log in
+export const authorize = async dispatch => {
+  try {
+    const user = await axios.get("/api/auth/me");
+    dispatch({
+      type: LOGIN_SUCCESS,
+      payload: Number(user.data.user)
+    });
+  } catch (err) {
+    console.log(err.message);
+  }
+};
 
 // log out
+export const logout = async dispatch => {
+  try {
+    dispatch({
+      type: LOGOUT
+    });
+    await axios.post("/api/users/logout");
+  } catch (err) {
+    console.log(err.message);
+  }
+};
 
 const initialState = {
   isAuth: false,
@@ -13,7 +51,21 @@ const initialState = {
 };
 
 const authReducer = (state = initialState, action) => {
-  switch (action.type) {
+  const { type, payload } = action;
+
+  switch (type) {
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        isAuth: true,
+        userId: payload
+      };
+    case LOGOUT:
+      return {
+        ...state,
+        isAuth: false,
+        userId: null
+      };
     default:
       return state;
   }
