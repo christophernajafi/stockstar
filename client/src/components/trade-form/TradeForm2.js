b08b3b96bca74bd887bcc2244adc817ce377c74d
Christopher Najafi <chris.najafi@gmail.com>
2020-03-05 21:03:14 -0500

    deleted unused components

--- client/src/components/trade-form/TradeForm2.js
+++ client/src/components/trade-form/TradeForm2.js
@@ -1,86 +1,79 @@
-import React, { Component, Fragment } from "react";
+import React, { Fragment, useState, useContext } from "react";
 import { Button, Form, Container } from "react-bootstrap";
 
 import AuthContext from "../../context/AuthContext";
 import "./trade-form.css";
 
 // Child component of Portfolio
-class TradeForm extends Component {
-  static contextType = AuthContext;
+const TradeForm = props => {
+  const authContext = useContext(AuthContext);
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      ticker: "",
-      quantity: 1
-    };
-  }
+  const [tradeFormState, setTradeFormState] = useState({
+    ticker: "",
+    quantity: 1
+  });
 
-  handleSubmit = async event => {
+  const { ticker, quantity } = tradeFormState;
+
+  const handleSubmit = async event => {
     event.preventDefault();
-    this.setState({ quantity: 1, ticker: "" });
+    setTradeFormState({ ...tradeFormState, quantity: 1, ticker: "" });
     // adding a transaction handled in parent component
-    await this.props.onSubmit(
-      this.state.ticker,
-      Number(this.state.quantity),
-      "Buy"
-    );
+    await props.onSubmit(ticker, Number(quantity), "Buy");
   };
 
-  render() {
-    // eslint-disable-next-line
-    const { userCashBalance, error } = this.props;
-    const validQuantity =
-      Number(this.state.quantity) > 0 &&
-      !String(this.state.quantity).includes(".");
-    const emptyTicker = this.state.ticker === "";
+  const { userCashBalance, error } = props;
+  const validQuantity = Number(quantity) > 0 && !String(quantity).includes(".");
+  const emptyTicker = ticker === "";
 
-    return (
-      <Fragment>
-        <Container>
-          Cash Balance: $
-          {userCashBalance.toLocaleString(undefined, {
-            minimumFractionDigits: 2,
-            maximumFractionDigits: 2
-          })}
+  return (
+    <Fragment>
+      <Container>
+        Cash Balance: $
+        {userCashBalance.toLocaleString(undefined, {
+          minimumFractionDigits: 2,
+          maximumFractionDigits: 2
+        })}
+        <br />
+        <Form onSubmit={handleSubmit}>
+          <Form.Group controlId="ticker">
+            <Form.Label>Ticker Symbol</Form.Label>
+            <Form.Control
+              name="ticker"
+              type="text"
+              placeholder="Enter ticker symbol"
+              onChange={event =>
+                setTradeFormState({
+                  ...tradeFormState,
+                  ticker: event.target.value
+                })
+              }
+              value={ticker}
+            />
+          </Form.Group>
           <br />
-          <Form onSubmit={this.handleSubmit}>
-            <Form.Group controlId="ticker">
-              <Form.Label>Ticker Symbol</Form.Label>
-              <Form.Control
-                name="ticker"
-                type="text"
-                placeholder="Enter ticker symbol"
-                onChange={event =>
-                  this.setState({ ...this.state, ticker: event.target.value })
-                }
-                value={this.state.ticker}
-              />
-            </Form.Group>
-            <br />
-            <Form.Group controlId="quantity">
-              <Form.Label>Number of Shares</Form.Label>
-              <Form.Control
-                name="quantity"
-                value={this.state.quantity}
-                type="text"
-                placeholder="Enter quantity"
-                onChange={event =>
-                  this.setState({
-                    ...this.state,
-                    quantity: event.target.value
-                  })
-                }
-              />
-            </Form.Group>
-            <Button type="submit" disabled={!validQuantity || emptyTicker}>
-              Buy
-            </Button>
-          </Form>
-        </Container>
-      </Fragment>
-    );
-  }
-}
+          <Form.Group controlId="quantity">
+            <Form.Label>Number of Shares</Form.Label>
+            <Form.Control
+              name="quantity"
+              value={quantity}
+              type="text"
+              placeholder="Enter quantity"
+              onChange={event =>
+                setTradeFormState({
+                  ...tradeFormState,
+                  quantity: event.target.value
+                })
+              }
+            />
+          </Form.Group>
+          <Button type="submit" disabled={!validQuantity || emptyTicker}>
+            Buy
+          </Button>
+        </Form>
+      </Container>
+    </Fragment>
+  );
+};
 
 export default TradeForm;
